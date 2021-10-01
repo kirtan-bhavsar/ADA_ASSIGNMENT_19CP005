56e1949642508d12f318be2ca575f247e4184c8e
Your Name <kirtanbhavsar.2491@gmail.com>
2021-10-01 17:36:15 +0530

    Bubble sort

--- BUBBLE_SORT.c
+++ BUBBLE_SORT.c
@@ -1,17 +1,36 @@
-#include<stdio.h>
-#include<conio.h>
+#include <stdio.h>
 
-void printArray(int* A, int n){
+
+void printArray(int* A, int n)
+{
     for (int i = 0; i < n; i++)
     {
-       printf("%d",A[i]);
+        printf("%d/n", A[i]);
     }
-    
 }
 
-int main{
-     int A[] = {41,8,32,17,69,5};
-     int n = 6;
-    printArray(A,n);
+void bubbleSort(int* A, int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - 1 - i; j++)
+        {
+            if (A[j] > A[j + 1])
+            {
+                int temp;
+                temp = A[j];
+                A[j]  = A[j+1];
+                A[j+1] = temp;
+            }
+        }
+    }
+}
+
+int main()
+{
+    int A[] = {41, 8, 32, 17, 69, 5};
+    int n = 6;
+    bubbleSort(A, n);
+    printArray(A, n);
     return 0;
 }
